3452aa0775a4f7e4e51834c61d40b2167eb8ea78
nicolas <n@baldeck.net>
2022-07-25 16:30:15 +0200

    mise en page formulaire * cgv

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,121 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Abonnement</h1>
+<p>Ici, vous pouvez souscrire ou renouveller un abonnement de communication pour une balise météo Sigfox (Pioupiou, MeteoWind, Arduino...)</p>
+
+<p>Le coût est de 20 € TTC pour un an. Il permet à la balise d'émettre depuis le réseau Sigfox vers OpenWindMap. La consultation et l'accès aux données restent gratuits pour tous.</p>
+
+<p>Pour des raisons techniques, le paiement ne peut se faire qu'en ligne, par Carte Bancaire ou Paypal. Si vous souhaitez prendre l'abonnement au nom d'une association qui n'a pas de carte bancaire, vous pouvez faire le paiement avec votre carte personelle. Nous fournissons systèmatiquement une facture, que vous pourrez présenter à votre association pour vous faire rembourser.</p>
+
+<form>
+
+  <h2>Quelle balise ?</h2>
+  
+  <div class="form-group">
+    <label for="station_id">Numéro de la balise</label>
+    <input type="text" class="form-control" id="station_id" placeholder="123...">
+  </div>
+  <div class="form-group">
+    <label for="station_name">Nom de la balise</label>
+    <input type="text" class="form-control" id="station_name" readonly>
+  </div>
+  <div class="form-group">
+    <label for="station_current_contract">Abonnement actuel</label>
+    <input type="text" class="form-control" id="station_current_contract" readonly>
+  </div>
+  
+  <h2>Vos coordonnées</h2>
+  <div class="form-group">
+    <label for="last_name">Nom</label>
+    <input type="text" class="form-control" id="last_name">
+  </div>
+  <div class="form-group">
+    <label for="first_name">Prénom</label>
+    <input type="text" class="form-control" id="first_name">
+  </div>
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+  
+  <h2>Facturation</h2>
+  <div class="form-group">
+    <label for="invoice_name">Nom de la personne ou club / association / entreprise</label>
+    <input type="text" class="form-control" id="invoice_name">
+  </div>
+  <div class="form-group">
+    <label for="invoice_street">Adresse</label>
+    <input type="text" class="form-control" id="invoice_street">
+  </div>
+  <div class="form-group">
+    <label for="invoice_postcode">Code postal</label>
+    <input type="text" class="form-control" id="invoice_postcode">
+  </div>
+  <div class="form-group">
+    <label for="invoice_city">Ville</label>
+    <input type="text" class="form-control" id="invoice_city">
+  </div>
+  <div class="form-group">
+    <label for="invoice_country">Pays</label>
+    <select class="form-control" id="invoice_country"></select>
+  </div>
+  
+  <h2>Paiement</h2>
+
+  <p><strong>20 € TTC</strong>, soit 16.67 € HT</p>
+
+  <div class="checkbox">
+    <label>
+      <input type="checkbox"> J'accepte les conditions de vente présentes au bas de cette page.
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Aller au paiement</button>
+</form>
+
+<p>&nbsp;</p>
+<hr>
+<p>&nbsp;</p>
+
+<h3>Mentions légales :</h3>
+
+<p>Société Coopérative d'Intérêt Collectif OpenWindMap SCIC SA<p>
+
+<p>350 CHE DU PRE NEUF, CDV 50068, 38350 LA MURE, FRANCE</p>
+
+<p>
+TVA Intracommunautaire : FR24903301737<br>
+R.C.S. Grenoble 903 301 737<br>
+Capital social variable, minimum de 18 500,00 Euros
+</p>
+
+<p>Contact : contact@openwindmap.org</p>
+
+<p>Directeur de la publication : Nicolas Baldeck</p>
+
+<p>Hébergeur : OVH SAS, 2 rue Kellermann, 59100 Roubaix, France</p>
+
+<h3>Conditions de vente</h3>
+
+<p>La SCIC SA OpenWindMap propose un abonnement permettant de faire communiquer une balise météo, depuis le réseau Sigfox, vers le site internet openwindmap.org</p>
+
+<p>Chaque balise doit disposer de son propre abonnement de communication, dont le prix est fixé à 20 € par an, toutes taxes comprises.</p>
+
+<p>Avant le paiement, le client indique le numéro de la balise pour laquelle il souhaite renouveller l'abonnement. OpenWindMap ne pourra pas rembourser un abonnement souscrit pour un mauvais numéro.</p>
+
+<p>Le paiement s'effectue depuis le site internet https://abo.openwindmap.org. Une facture sera ensuite adressée par email. Le client doit s'assurer de fournir les bonnes coordonnées de facturation</p>
+
+<p>L'abonnement inclus les frais de communication à travers le réseau Sigfox, pour un maximum de 144 messages / jour, avec une itinérance possible dans les pays Européens couverts par Sigfox. L'abonnement inclus également les frais occasionnés pour le traitement et l'archivage des données sur les serveurs de OpenWindMap.</p>
+
+<p>Après le paiement, l'abonnement devient activable : soit immédiatement si aucun abonnement n'était en cours de validité pour cet appareil, soit dès que l'abonnement précédent prend fin. L'abonnement est ensuite activé dès qu'un message émis par l'appareil est capté par le réseau Sigfox. Il est ensuite valable 365 jours à compter de la date d'activation.</p>
+
+<p>Le client peut demander l'annulation et le remboursement pendant une période de 3 mois après le paiement, tant que l'abonnement n'a pas encore été activé. Dès lors que l'abonnement a été activé (donc qu'un message a été reçu), plus aucun remboursement n'est possible, y compris en cas de panne ou vol de l'appareil.</p>
+
+<p>OpenWindMap fera de son mieux pour assurer le fonctionnement du réseau. Cependant, OpenWindMap ne peut donner aucune garantie de disponibilité ou de fiabilité du réseau. Également, OpenWindMap ne peut donner aucune garantie en ce qui concerne la couverture du réseau Sigfox.</p>
+
+<p>OpenWindMap peut résilier un abonnement à tout moment, sans avoir à fournir de justification. Dans ce cas, un remboursement sera effectué, au prorata du temps d'abonnement restant. Le client ne pourra prétendre à aucune autre indemnisation.</p>
+
+<p>En connectant votre appareil au réseau OpenWindMap, vous autorisez OpenWindMap à redistribuer ses mesures, sa localisation et ses métadonées, sous toutes formes et sous toutes licences, pour tout usage que ce soit-</p>
+
+<p>Toute demande d'aide ou réclamation doit être adressée prioritairement sur le forum disponible à l'adresse https://forum.openwindmap.org, ou par email à contact@openwindmap.org</p>
+
+<p>Les présentes conditions sont valables au moment du paiement et jusqu'à la date de fin de l'abonnement. OpenWindMap se réserve le droit de les modifier, après en avoir informé le client. Dans le cas où le client n'accepterait pas les nouvelles conditions, OpenWindMap pourra procéder à la résiliation et au remboursement prévus plus haut.</p>
+
+<p>Ce contrat est régit par la loi Française. En cas de litige, seul le Tribunal de Commerce de Grenoble sera compétent.</p>
